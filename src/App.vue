d5c3da4fbc4c48c9d8626abf810c99e5d59d9c6c
Mippoose Rust <mippoose.m1l@gmail.com>
2021-03-03 18:05:33 +0600

    created!

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <Creator></Creator>
+</template>
+
+<script>
+import { computed } from 'vue'
+import Creator from "./components/creator/index.vue"
+export default {
+  name: 'App',
+  data(){
+    return{
+      ActiveComponents: [],
+    }
+  },
+  components: {
+    Creator
+  },
+  methods:{
+
+  },
+  computed: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/creator/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>{{text}}</h1>
+    <button @click="Clicked()">CLICK ME</button>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            text: "Hello world!",
+            i: 1,
+        }       
+    },
+    methods:{
+        Clicked(){
+            this.i++;
+            this.text = this.i;
+        },
+    },
+}
+</script>
